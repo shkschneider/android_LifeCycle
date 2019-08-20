ce7362b566f6db5f8e5680d12c16f5632539d453
shkschneider <shk.schneider@gmail.com>
2019-08-20 14:42:50 +0200

    Activity

---
+++ README.md
@@ -0,0 +1,14 @@
+2019-08-20 14:40:53.908 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: init
+2019-08-20 14:40:53.909 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: attachBaseContext
+2019-08-20 14:40:54.002 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onCreate
+2019-08-20 14:40:54.004 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onStart
+2019-08-20 14:40:54.011 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onResume
+
+2019-08-20 14:41:33.219 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onPause
+2019-08-20 14:41:33.226 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onStop
+2019-08-20 14:41:33.228 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onDestroy
+2019-08-20 14:41:33.246 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: init
+2019-08-20 14:41:33.247 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: attachBaseContext
+2019-08-20 14:41:33.290 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onCreate
+2019-08-20 14:41:33.291 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onStart
+2019-08-20 14:41:33.295 16448-16448/me.shkschneider.lifecycle V/LifeCycle:MainActivity: onResume
--- app/src/main/java/me/shkschneider/lifecycle/MainActivity.kt
+++ app/src/main/java/me/shkschneider/lifecycle/MainActivity.kt
@@ -1,13 +1,63 @@
 package me.shkschneider.lifecycle
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+private val TAG = "LifeCycle:MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    init {
+        Log.v(TAG, "init")
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        Log.v(TAG, "attachBaseContext")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.v(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        restart.setOnClickListener { recreate() }
+        finish.setOnClickListener { finish() }
+    }
+
+    override fun onRestart() {
+        Log.v(TAG, "onRestart")
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        Log.v(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.v(TAG, "onResume")
+        super.onResume()
+    }
+
+    // RUNNING
+
+    override fun onPause() {
+        Log.v(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.v(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.v(TAG, "onDestroy")
+        super.onDestroy()
     }
 
 }
